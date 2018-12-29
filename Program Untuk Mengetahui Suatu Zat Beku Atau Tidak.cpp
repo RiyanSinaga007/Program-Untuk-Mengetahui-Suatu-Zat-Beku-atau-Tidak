bb3d44e97a492cd65cffc7e45f79af2b5d659148
RiyanSinaga007 <45660151+RiyanSinaga007@users.noreply.github.com>
2018-12-30 01:47:23 +0700

    Add files via upload

---
+++ Program Untuk Mengetahui Suatu Zat Beku Atau Tidak.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int a;
+	
+	cout<<"MASUKAN DERAJAT SUATU ZAT =";
+	cin>>a;
+	if(a>0)
+	cout<<"SUHU"<<a<<" DERAJAT ZAT BEKU ";
+	else
+	cout<<"SUHU"<<a<<" DERAJAT ADALAH ZAT TIDAK BEKU ";
+	return 0;
+}
